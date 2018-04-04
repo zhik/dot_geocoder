41bb543e50219b6392a9834cc60ed204efba71ff
zhik <zhikhe@gmail.com>
2018-04-04 16:28:30 -0400

    fixing npm - shp-write

--- src/helpers/shp-write/src/download.js
+++ src/helpers/shp-write/src/download.js
@@ -2,5 +2,5 @@ var zip = require('./zip');
 
 module.exports = function(gj, options) {
     var content = zip(gj, options);
-    location.href = 'data:application/zip;base64,' + content;
+    window.location.href = 'data:application/zip;base64,' + content; 
 };
