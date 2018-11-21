1db891e8206c2c36e21898a962c470fe6607d190
zhik <zhikhe@gmail.com>
2018-11-21 11:16:13 -0500

    added IE message

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './css/index.css';
 import Router from './Router';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
+if(typeof ['test'].includes !== 'undefined'){
+    ReactDOM.render(<Router />, document.getElementById('root'));
+    registerServiceWorker();
+}else{
+    document.getElementById('root').innerHTML = '<div class="ui warning message">App doesn\'t support your browser, use modern browsers like Chrome or Firefox.</div>'
+}
+
